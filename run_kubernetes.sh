ee15d960785b178dcda14b6f98c23565572e40a4
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2022-08-23 11:45:43 +0100

    updated files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,14 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=goodiec/mlproject
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mlproject --image=$dockerpath
 
+# list the pods
+kubectl get pods
 
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlproject 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=goodiec/mlproject
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login
+docker tag mlproject $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
